cc29f180acba06f5b331567765992a6f2c4b477b
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-09-24 19:55:07 -0300

    a

--- main.js
+++ main.js
@@ -17,32 +17,31 @@ function media(number1,number2,number3){
 }
 
 function bhaskara(a, b, c) {
-   
     let delta = b * b - 4 * a * c;
 
-   
     if (delta < 0) {
         return "Não existem raízes reais";
     } else if (delta === 0) {
-        
         let x = -b / (2 * a);
         return `Raiz única: x = ${x}`;
     } else {
-       
-        let x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        let x2 = (-b - Math.sqrt(delta)) / (2 * a);
-        return `As raízes são: x1 = ${x1}, x2 = ${x2}`;
+        let raizDelta = Math.sqrt(delta);
+        let x1 = (-b + raizDelta) / (2 * a);
+        let x2 = (-b - raizDelta) / (2 * a);
+
+        if (Number.isInteger(raizDelta)) {
+            return `As raízes são: x1 = ${x1}, x2 = ${x2}`;
+        } else {
+            return `As raízes são: x1 = (-${b} + √${delta}) / ${2 * a}, x2 = (-${b} - √${delta}) / ${2 * a}`;
+        }
     }
 }
 
-
 let input = prompt('Digite os valores de a, b e c separados por vírgulas (ex: 1, -3, 2)');
-
-
 let valores = input.split(',').map(Number);
-
-let [a, b, c] = valores; 
+let [a, b, c] = valores;
 let resultado = bhaskara(a, b, c);
 document.write(resultado);
+
 
 
