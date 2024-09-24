3ae3821ba85435903c31de90f8a3c243920810f8
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-09-24 12:21:28 -0300

    a

--- main.js
+++ main.js
@@ -15,9 +15,7 @@ console.log(areaTrapezio(10,10,10));
 function media(number1,number2,number3){
     return(number1+number2+number3)/3;
 }
-parseInt(prompt('Média de 3 números')){
-return(document.write(media))
-}
+parseInt(prompt('Média de 3 números'))a
 
     
 
