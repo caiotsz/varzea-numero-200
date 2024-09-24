ca4447a2ca3f717ff9a0b13e1bbe68fdfa348525
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-09-24 11:54:40 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function soma(number1,number2){
     return number1+number2;
 }
-console.log(soma(20,30));
+console.log(soma(10,10));
 function areaQuadrado(number1){
 return number1*number1;
 }
+console.log(areaQuadrado(10))
 let areaTrapezio=function(base1,base2,altura){
-    return (base1+base2)*altura/2
+    return (base1+base2)*altura/2;
 }
+console.log(areaTrapezio(10,10,10));
