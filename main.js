d6cfa1c6842939b510324a7188223cff781c3b04
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-09-24 12:18:22 -0300

    .

--- main.js
+++ main.js
@@ -10,9 +10,11 @@ let areaTrapezio=function(base1,base2,altura){
     return (base1+base2)*altura/2;
 }
 console.log(areaTrapezio(10,10,10));
+
+
 function media(number1,number2,number3){
-    return(number1+number2+number3)/3
+    return(number1+number2+number3)/3;
 }
 parseInt(prompt('Média de 3 números'))
-    document.write(media)
+    
 
