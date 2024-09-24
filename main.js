6627988b8fbe44f19d4f46afd297fbf1d7d3e6dd
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-09-24 19:58:20 -0300

    a

--- main.js
+++ main.js
@@ -19,21 +19,18 @@ function media(number1,number2,number3){
 function bhaskara(a, b, c) {
     let delta = b * b - 4 * a * c;
 
-    if (delta < 0) {
-        return "Não existem raízes reais";
-    } else if (delta === 0) {
-        let x = -b / (2 * a);
-        return `Raiz única: x = ${x}`;
-    } else {
+    if (delta > 0) {
         let raizDelta = Math.sqrt(delta);
         let x1 = (-b + raizDelta) / (2 * a);
         let x2 = (-b - raizDelta) / (2 * a);
-
-        if (Number.isInteger(raizDelta)) {
-            return `As raízes são: x1 = ${x1}, x2 = ${x2}`;
-        } else {
-            return `As raízes são: x1 = (-${b} + √${delta}) / ${2 * a}, x2 = (-${b} - √${delta}) / ${2 * a}`;
-        }
+        return `As raízes são: x1 = ${x1.toFixed(2}, x2 = ${x2.toFixed(2)}`;
+    } else if (delta === 0) {
+        let x = -b / (2 * a);
+        return `Raiz única: x = ${x.toFixed(2)}`;
+    } else {
+        let parteReal = (-b / (2 * a)).toFixed(2);
+        let parteImaginaria = (Math.sqrt(-delta) / (2 * a)).toFixed(2);
+        return `As raízes são: x1 = ${parteReal} + ${parteImaginaria}i, x2 = ${parteReal} - ${parteImaginaria}i`;
     }
 }
 
@@ -42,6 +39,7 @@ let valores = input.split(',').map(Number);
 let [a, b, c] = valores;
 let resultado = bhaskara(a, b, c);
 document.write(resultado);
+
 
 
 
