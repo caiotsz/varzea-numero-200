f3b0bd4dd7bd9d00e666714ca91b3a85c84b4549
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-09-24 11:43:53 -0300

    .

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function soma(number1,number2){
     return number1+number2;
 }
 console.log(soma(20,30));
-function area(number1,number1){
+function areaQuadrado(number1){
 return number1*number1;
 }
